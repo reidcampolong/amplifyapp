03be5d20812dc4f4691c566ab988090372768cff
Reid Campolong <reidcampolong@gmail.com>
2020-09-15 22:41:56 -0400

    Site update changes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <h1>Welcome to the site poggers</h1>
           Learn React
         </a>
       </header>

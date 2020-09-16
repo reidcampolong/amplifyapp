db78347b2a6b4be446b2d5515a2c5f1903bf44e4
Reid Campolong <reidcampolong@gmail.com>
2020-09-15 22:45:59 -0400

    New changes for viewing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Welcome to the site poggers</h1>
-          Learn React
-        </a>
-      </header>
+      <p>Hey hey hey</p>
+      <h1>Welcome to the site poggers</h1>
     </div>
   );
 }
